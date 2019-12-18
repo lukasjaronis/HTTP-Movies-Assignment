44a4a3de864b87d41a3d483821c01c810238f6ce
lukasjaronis <jaronisluke13@gmail.com>
2019-12-18 16:31:58 -0500

    movie.js done

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -31,6 +31,17 @@ export default class Movie extends React.Component {
     addToSavedList(this.state.movie);
   };
 
+  handleDelete = (e, props) => {
+    e.preventDefault();
+    axios
+    .delete('http://localhost:5000/api/movies/${this.state.movie.id}')
+    .then(response => {
+      this.props.setMovies(response.data);
+      this.props.history.push('/');
+    })
+    .catch(error => console.log(error));
+  }
+
   render() {
     if (!this.state.movie) {
       return <div>Loading movie information...</div>;
@@ -42,6 +53,8 @@ export default class Movie extends React.Component {
         <div className="save-button" onClick={this.saveMovie}>
           Save
         </div>
+        <button onClick={() => this.props.history.push(`/update-movie/${this.state.movie.id}`)}>Edit</button>
+        <button  onClick={this.handleDelete}>Delete</button>
       </div>
     );
   }
