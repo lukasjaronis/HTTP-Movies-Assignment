623c86f3f5f0180ce7b63f35cebc04ed04c2a153
lukasjaronis <jaronisluke13@gmail.com>
2019-12-18 17:40:29 -0500

    delete and update working

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,5 +1,6 @@
 import React from "react";
 import axios from "axios";
+import { Link } from "react-router-dom";
 import MovieCard from "./MovieCard";
 export default class Movie extends React.Component {
   constructor(props) {
@@ -21,9 +22,9 @@ export default class Movie extends React.Component {
 
   fetchMovie = id => {
     axios
-      .get(`http://localhost:5000/api/movies/${id}`)
+      .get(`http://localhost:5000/api/movies/${id}`) // fetches movies with ID
       .then(res => this.setState({ movie: res.data }))
-      .catch(err => console.log(err.response));
+      .catch(err => console.log(err));
   };
 
   saveMovie = () => {
@@ -31,17 +32,31 @@ export default class Movie extends React.Component {
     addToSavedList(this.state.movie);
   };
 
+  handleDelete = e => {
+    e.preventDefault();
+    axios
+      .delete(`http://localhost:5000/api/movies/${this.props.match.params.id}`) // gets the id of the movie in the browser link
+      .then(res => {
+        console.log("res", res);
+        this.props.history.push("/"); // .then after it pushes to /home
+      })
+      .catch(err => console.log(err));
+  };
+
   render() {
     if (!this.state.movie) {
       return <div>Loading movie information...</div>;
     }
-
     return (
       <div className="save-wrapper">
         <MovieCard movie={this.state.movie} />
         <div className="save-button" onClick={this.saveMovie}>
           Save
         </div>
+        <button>
+          <Link to={`/update-movie/${this.state.movie.id}`}>Update Movie</Link>
+        </button>
+        <button onClick={this.handleDelete}>Delete</button>
       </div>
     );
   }
